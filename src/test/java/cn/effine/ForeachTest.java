163257ee05daa6f3e0561a62251c21004d848103
effine <zhangyafei@co-mall.com>
2016-09-09 16:07:33 +0800

    test

--- src/test/java/cn/effine/ForeachTest.java
+++ src/test/java/cn/effine/ForeachTest.java
@@ -1,10 +1,13 @@
 package cn.effine;
 
+import java.math.BigDecimal;
+
 public class ForeachTest {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+		BigDecimal c1 = new BigDecimal(0.01);
+		System.out.println(c1.compareTo(BigDecimal.ZERO)==1);
+				
 	}
 }
